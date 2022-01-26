1c9d9cda35d55c4af34fb967a303c42d77fe09fe
HidetoNiwa <tototohide@gmail.com>
2022-01-26 18:55:53 +0900

    #5 delete curl push

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -9,23 +9,3 @@ pbibtex ./main
 platex -kanji=utf8 -synctex=1 -interaction=nonstopmode ./main.tex
 platex -kanji=utf8 -synctex=1 -interaction=nonstopmode ./main.tex
 dvipdfmx -f ptex-ipa.map -I 24 main.dvi
-
-today=`date "+%Y%m%d%H%M%S"`
-# create release
-res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
--d "
-{
-  \"tag_name\": \"$today\",
-  \"target_commitish\": \"$GITHUB_SHA\",
-  \"name\": \"$today\",
-  \"draft\": false,
-  \"prerelease\": false
-}"`
-
-# extract release id
-rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
-
-# upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
---header 'Content-Type: application/pdf'\
---upload-file main.pdf
