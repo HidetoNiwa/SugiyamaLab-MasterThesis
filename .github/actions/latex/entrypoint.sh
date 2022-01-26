df2a14314b82e20f23dd39240c392280e707558c
HidetoNiwa <tototohide@gmail.com>
2022-01-26 17:26:50 +0900

    #5 fix github actions

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
+
+# . = /github/workspace if actions/checkout
 set -eux
 
-# build pdf (change if necessary)
-platex ./main.tex
+if [ ! -f .latexmkrc ]; then
+    cp /.latexmkrc .latexmkrc
+fi
 
-platex ./main.tex
-
-platex ./main.tex
-
-dvipdfmx -f ptex-ipa.map main
-today=`date "+%Y%m%d%H%M%S"`
-# create release
-res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
--d "
-{
-  \"tag_name\": \"$today\",
-  \"target_commitish\": \"$GITHUB_SHA\",
-  \"name\": \"$today\",
-  \"draft\": false,
-  \"prerelease\": false
-}"`
-
-# extract release id
-rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
-
-# upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
---header 'Content-Type: application/pdf'\
---upload-file main.pdf
+# make pdf
+latexmk $INPUT_LATEX_FILE_NAME
